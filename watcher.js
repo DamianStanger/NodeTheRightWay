b009068962ccc2f1b6736f69cf77a3dd392403d7
damo <damo@localhost.localdomain>
2016-11-06 14:31:46 +0000

    added spawn from the child_process module

--- watcher.js
+++ watcher.js
@@ -1,5 +1,13 @@
-const fs=require('fs');
-fs.watch('target.txt', function(){
-    console.log("File 'target.txt' just changed");
+const fs=require('fs'),
+    spawn = require('child_process').spawn,
+    filename = process.argv[2]
+
+if (!filename){
+    throw Error("a file to watch must be specified");
+}
+
+fs.watch(filename, function(){
+    let ls = spawn('ls', ['-lh', filename]);
+    ls.stdout.pipe(process.stdout);
 });
 console.log("Fin");
